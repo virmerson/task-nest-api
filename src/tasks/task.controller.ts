5349019113c84e4dcf29079e608e2e4f2c327748
@virmerson <virmerson@htcursos.com>
2024-07-15 01:01:50 +0100

    Crud Basico

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common'
 import { TaskService } from './task.service'
 import { Task } from './task.model'
 
@@ -12,8 +12,24 @@ export class TaskController {
         return this.taskService.getAllTasks()
     }
 
+    @Get(':id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(+id)
+    }
+    @Patch(':id')
+    updateTask(@Param('id') id: string, @Body() taskUpdate: Partial<Task>): Task {
+        return this.taskService.updateTask(+id, taskUpdate)
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id: string): void {
+        this.taskService.deleteTask(+id)
+    }
     @Post()
     createTask(@Body('title') title: string): Task {
         return this.taskService.createTask(title)
     }
+
+
+
 }
--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,13 +1,63 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { Task } from "./task.model";
 
 @Injectable()
-export class TaskService {
-    private tasks: Task[] = []
+export class TaskService implements OnModuleInit {
 
+    private tasks: Task[] = [];
+
+    onModuleInit() {
+        //     if (this.tasks.length === 0)
+        //         this.initTasks()
+    }
+
+    // initTasks() {
+    //     Array.from({ length: 10 }, (_, index) => {
+    //         this.createTask(`Task ${index + 1}`)
+    //     })
+    // }
+    //complexity O(n)
+    deleteTask(id: number): void {
+        const taskIndex = this.tasks.findIndex(task => task.id === id)
+        if (taskIndex === -1) {
+            throw new Error('Task not found');
+        }
+        this.tasks.splice(taskIndex, 1)
+
+    }
+
+    //complexity O(n)
+    updateTask(id: number, taskUpdate: Partial<Task>): Task {
+
+        const taskIndex = this.tasks.findIndex(task => task.id === id)
+
+        if (taskIndex === -1) {
+            throw new Error('Task not found');
+        }
+
+        this.tasks[taskIndex] = { ...this.tasks[taskIndex], ...taskUpdate }
+        return this.tasks[taskIndex]
+    }
+
+
+    //complexity O(n)
+    getTaskById(id: number): Task {
+        const task = this.tasks.find(task => task.id === id)
+        console.log(task)
+        console.log(id)
+        console.log(typeof id)
+        if (!task) {
+            throw new Error('Task not found')
+        }
+        return task;
+    }
+
+    //complexity O(1)
     getAllTasks(): Task[] {
         return this.tasks;
     }
+
+    //complexity O(1)
     createTask(title: string): Task {
         const newTask: Task = {
             id: Date.now(),
@@ -17,5 +67,7 @@ export class TaskService {
         this.tasks.push(newTask);
         return newTask;
     }
+
+
 
 }
